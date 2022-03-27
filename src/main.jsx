db1505ff5a56d80cc317d524de1fd3ec0aaa2d0e
Fhasghrge <1969533391@qq.com>
2022-03-27 23:09:09 +0800

    remove watermarker

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter } from 'react-router-dom'
-import watermark from './utils'
 import './index.css'
 import App from './App'
 
-watermark.set('测试文件')
 
 ReactDOM.render(
   <HashRouter>
